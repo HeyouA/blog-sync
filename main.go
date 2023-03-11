89888af318645fd58ec4888a1113876825546943
weiluoliang <625753396@qq.com>
2023-03-11 23:12:54 +0800

    fix

--- main.go
+++ main.go
@@ -13,8 +13,19 @@ func main() {
 	r.GET("/sync", func(c *gin.Context) {
 		// 去同步我的文件
 		go func() {
-			cmd := exec.Command("git", "pull")
-			cmd.Dir = "/var/www/blog"
+
+			// 删除目录
+			rm := exec.Command("rm", "-rf", "/usr/share/nginx/weiluoliang.github.io")
+			output, err2 := rm.CombinedOutput()
+			fmt.Printf("delete result :\n%s\n", string(output))
+			if err2 != nil {
+				log.Printf("sync failed with %s\n", err2)
+			}
+
+			// check 指定分支
+			//  git clone -b gh-page https://github.com/weiluoliang/weiluoliang.github.io.git
+			cmd := exec.Command("git", "clone", "-b", "gh-page", "https://github.com/weiluoliang/weiluoliang.github.io.git")
+			cmd.Dir = "/usr/share/nginx"
 			out, err := cmd.CombinedOutput()
 			fmt.Printf("sync result :\n%s\n", string(out))
 			if err != nil {
